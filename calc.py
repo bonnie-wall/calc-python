5738c359238bace1b45d88e333642dbcdc792cf5
Bonnie Wall <bonnie.margaret.wall@gmail.com>
2017-10-10 15:49:05 +1100

    fixed comma, added division

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 import sys
 
-filename, num1, op num2 = sys.argv
+filename, num1, op, num2 = sys.argv
 num1, num2 = int(num1), int(num2)
 
 if op == '+':
@@ -12,6 +12,8 @@ elif op =="-":
 elif op =="*":
 	# now includes multiplication
 	print(num1 * num2)
+elif op =="/":
+	print(num1 / num2)
 else:
 	print "0"
 ## end of file
